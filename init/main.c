f1dcf86a5fe96f89bd4dedfe6c4f6e5d23d0d54d
moehanabi <moehanabichan@outlook.com>
2024-01-11 16:33:09 +0800

    feat: Add PIC & PIT test

--- init/main.c
+++ init/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <pic.h>
+#include <pit.h>
 
 int main(void){
     kprintf("In Long Mode\n");
+    pic_init(0x11, 0x20, 0x4, 0x3, 0x28);
+    pic_unmask(0);
+    pit_set(2, 0, 1000);
 }
