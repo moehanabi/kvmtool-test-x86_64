2ff3e2d4932c7d7a3feef04ba6328b6fdcea1432
moehanabi <moehanabichan@outlook.com>
2024-01-15 19:27:47 +0800

    feat: Add readeflags function

--- include/x86.h
+++ include/x86.h
@@ -1,5 +1,12 @@
 #ifndef X86_H
 #define X86_H
+#include <stddef.h>
+
+static inline uint32_t readeflags(void) {
+    uint64_t eflags;
+    __asm__ volatile("pushfq; pop %0" : "=r"(eflags));
+    return (uint32_t)eflags;
+}
 
 static inline void reboot(void) {
     __asm__ volatile(
