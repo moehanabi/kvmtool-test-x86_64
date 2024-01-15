089d8ed0e6d672d684cf454f474ffc326688b2d8
moehanabi <moehanabichan@outlook.com>
2024-01-15 16:51:38 +0800

    feat: Add pit cycle config
    Print pit message for 10 times.
    
    Attention: the frequency of pit can not be too low, otherwise the
    divisor will overflow.

--- interrupt/trap.c
+++ interrupt/trap.c
@@ -2,10 +2,15 @@
 #include <traps.h>
 #include <interrupt.h>
 #include <stdio.h>
+#include <pit.h>
+#include <x86.h>
 
 // Interrupt descriptor table (shared by all CPUs).
 struct gatedesc idt[256];
 extern uint64_t vectors[];  // in vectors.S: array of 256 entry pointers
+
+#define PLANED_TICK_NUM 1000
+static int ticks = 0;
 
 void tvinit(void) {
 	int i;
@@ -30,6 +35,12 @@ void trap(struct trapframe *tf) {
 
 	switch (tf->trapno) {
 	case T_IRQ0 + IRQ_TIMER:
+		if (++ticks % PLANED_TICK_NUM == 0) {
+			kprintf("%u ticks\n", ticks);
+			if (++ticks / PLANED_TICK_NUM == 10){
+				reboot();
+			}
+		}
 		break;
 	default:
 		kprintf("unexpected trap %u from rip %x\n", tf->trapno, tf->rip);
